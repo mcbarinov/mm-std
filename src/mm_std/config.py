297cc00010259e4574d7fd105076825dcfecc700
mcbarinov <mcbarinov@proton.me>
2025-01-20 15:04:46 +0300

    Redo BaseConfig.read_config #6

--- src/mm_std/config.py
+++ src/mm_std/config.py
@@ -1,12 +1,11 @@
 import io
-import sys
 from pathlib import Path
 from typing import Self
 
 import yaml
 from pydantic import BaseModel, ConfigDict, ValidationError
 
-from .print_ import PrintFormat, print_console, print_json, print_plain, print_table
+from .result import Err, Ok, Result
 from .str import str_to_list
 from .zip import read_text_from_zip_archive
 
@@ -31,47 +30,27 @@ class BaseConfig(BaseModel):
         return v
 
     @classmethod
-    def read_config(  # nosec: B107
-        cls,
-        config_path: io.TextIOWrapper | str | Path,
-        error_print_type: PrintFormat = PrintFormat.PLAIN,
-        zip_password: str = "",
-    ) -> Self:
+    def read_config(cls, config_path: io.TextIOWrapper | str | Path, zip_password: str = "") -> Result[Self]:  # nosec
         try:
             # is it zip archive?
             if isinstance(config_path, str) and config_path.endswith(".zip"):
                 config_path = str(Path(config_path).expanduser())
-                return cls(**yaml.full_load(read_text_from_zip_archive(config_path, password=zip_password)))
+                return Ok(cls(**yaml.full_load(read_text_from_zip_archive(config_path, password=zip_password))))
             if isinstance(config_path, io.TextIOWrapper) and config_path.name.endswith(".zip"):
                 config_path = str(Path(config_path.name).expanduser())
-                return cls(**yaml.full_load(read_text_from_zip_archive(config_path, password=zip_password)))
+                return Ok(cls(**yaml.full_load(read_text_from_zip_archive(config_path, password=zip_password))))
             if isinstance(config_path, Path) and config_path.name.endswith(".zip"):
                 config_path = str(config_path.expanduser())
-                return cls(**yaml.full_load(read_text_from_zip_archive(config_path, password=zip_password)))
+                return Ok(cls(**yaml.full_load(read_text_from_zip_archive(config_path, password=zip_password))))
 
             # plain yml file
             if isinstance(config_path, str):
-                return cls(**yaml.full_load(Path(config_path).expanduser().read_text()))
+                return Ok(cls(**yaml.full_load(Path(config_path).expanduser().read_text())))
             if isinstance(config_path, Path):
-                return cls(**yaml.full_load(config_path.expanduser().read_text()))
+                return Ok(cls(**yaml.full_load(config_path.expanduser().read_text())))
 
-            return cls(**yaml.full_load(config_path))
+            return Ok(cls(**yaml.full_load(config_path)))
         except ValidationError as err:
-            print_plain("config validation errors", error_print_type)
-            json_errors = []
-            rows = []
-            for e in err.errors():
-                loc = e["loc"]
-                field = ".".join(str(lo) for lo in loc) if len(loc) > 0 else ""
-                print_plain(f"{field} {e['msg']}", error_print_type)
-                json_errors.append({field: e["msg"]})
-                rows.append([field, e["msg"]])
-            print_table("config validation errors", ["field", "message"], rows)
-            print_json({"errors": json_errors}, error_print_type)
-            sys.exit(1)
+            return Err("validator error", data={"validaton_errors": err})
         except Exception as err:
-            if error_print_type == "json":
-                print_json({"exception": str(err)})
-            else:
-                print_console(f"config error: {err!s}")
-            sys.exit(1)
+            return Err(err)
