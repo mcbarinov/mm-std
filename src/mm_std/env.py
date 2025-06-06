7f4d28fb745b6842017d72c4a35fe641649f0b6b
mcbarinov <mcbarinov@proton.me>
2025-06-06 15:38:21 +0300

    Start v0.5 from scratch

--- src/mm_std/env.py
+++
@@ -1,9 +0,0 @@
-import os
-
-from dotenv import load_dotenv
-
-load_dotenv()
-
-
-def get_dotenv(key: str) -> str | None:
-    return os.getenv(key)
--- src/mm_std/types_.py
+++
@@ -1,8 +0,0 @@
-from collections.abc import Awaitable, Callable
-from typing import Any
-
-type CallableAny = Callable[..., Any]
-type Func = Callable[..., object]
-type AsyncFunc = Callable[..., Awaitable[object]]
-type Args = tuple[object, ...]
-type Kwargs = dict[str, object]
