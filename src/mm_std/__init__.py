9e4818e40d1a1a1eb2d80fd8d7ce9242cf9e3e31
mcbarinov <mcbarinov@proton.me>
2025-06-06 19:04:30 +0300

    dict_utils

--- src/mm_std/__init__.py
+++ src/mm_std/__init__.py
@@ -1,10 +1,12 @@
 from .date_utils import parse_date, utc_delta, utc_now
+from .dict_utils import replace_empty_dict_entries
 from .random_utils import random_datetime, random_decimal
 
 __all__ = [
     "parse_date",
     "random_datetime",
     "random_decimal",
+    "replace_empty_dict_entries",
     "utc_delta",
     "utc_now",
 ]
