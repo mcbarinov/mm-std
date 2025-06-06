3657000a66c0c51fb81dcbb74231f970367a8dd3
mcbarinov <mcbarinov@proton.me>
2025-06-06 17:19:27 +0300

    random_utils

--- src/mm_std/__init__.py
+++ src/mm_std/__init__.py
@@ -1,7 +1,10 @@
 from .date_utils import parse_date, utc_delta, utc_now
+from .random_utils import random_datetime, random_decimal
 
 __all__ = [
     "parse_date",
+    "random_datetime",
+    "random_decimal",
     "utc_delta",
     "utc_now",
 ]
